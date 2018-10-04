d4f85cdf56e4bf76834f665a677c703ca57d37c4
andyandpandy <andreas.beckerbertelsen@hotmail.com>
2018-10-04 20:55:36 +0200

    Add version with textual

--- MindstomCLI/customerService.py
+++ MindstomCLI/customerService.py
@@ -6,7 +6,7 @@ import requests
 class Service:
     def __init__(self):
         self.endpoint = "https://3xef1f6fnc.execute-api.eu-west-1.amazonaws.com/Testing/commandsfromshopper"
-        self.productIDS = [10851, 60156, 76093, 41594, 75193]
+        self.productIDS = ["10851", "60156", "76093", "41594", "75193"]
         print("Welcome to the LEGO store")
         self.name = input("What is your name? ")
         print("The Lego store has in total 5 items.")
@@ -21,6 +21,8 @@ class Service:
                 self.id_option()
             elif options == "2":
                 self.characteristics_option()
+            elif options == "3":
+                self.textual_option()
 
             else:
                 print("That is not a number in the range of 1-5")
@@ -39,24 +41,30 @@ class Service:
         try:
             print("Which of the following products would you like?")
             print(self.productIDS)
-            pID = int(input("Please type the product ID you would like: "))
+            pID = input("Please type the product ID you would like: ")
             if pID in self.productIDS:
-                self.productID = pID
-                self.send_request_with_id()
+                self.send_request_with_id(pID)
             else:
                 print("This id is not one of the following: ", self.productIDS)
         except Exception as e:
-            print(e.with_traceback())
             print("This is not an ID!")
+            print(e)
 
     def characteristics_option(self):
         try:
             age = input("How old are you?")
             hobbies = input("What hobbies interest you?")
             self.send_request({"name": self.name, "msg": "", "age": str(age), "hobbies": hobbies})
-        except Exception as e:
-            print(e.with_traceback())
-            print("This is not an ID!")
+        except Exception:
+            print("Something went wrong with the characteristics!")
+
+
+    def textual_option(self):
+        try:
+            description = input("Try and describe the product you are searching for")
+            self.send_request({"name": self.name, "msg": description})
+        except Exception:
+            print("Something with your description went wrong")
 
     def send_request(self, data):
         response = requests.post(self.endpoint, json.dumps(data))
